71b6edee7fa69fef3ff21453a1222beda82d6949
Gulsher Khan Zai <kzai659@yahoo.com>
2023-04-06 08:21:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function main() {
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, process.env.PUBLIC_DIR)))
+app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
 
 app.use('/', Rout.blog)
 app.use('*', (req, res) => {
