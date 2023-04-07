3efb8965e641b4d03ae19531ab4028a74ed38376
Gulsher Khan Zai <kzai659@yahoo.com>
2023-04-07 07:51:51 +0500

    vercel config

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
 
-app.use('/', Rout.blog)
+app.use('/api', Rout.blog)
 app.use('*', (req, res) => {
     res.send(path.resolve(__dirname, process.env.PUBLIC_DIR, 'index.html'))
 })
